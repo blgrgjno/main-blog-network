297e8c21bfcebc276181a41a60d17cde60694986
gorm <goer@dss.dep.no>
2013-12-20 15:19:41 +0100

    Adding livereload on .dev

--- themes/prioriteringsutvalget/functions.php
+++ themes/prioriteringsutvalget/functions.php
@@ -1,14 +1,17 @@
 <?php
 
-
 /**
- * Change logo image
+ * Adding livereload - if on a .dev domain
+ * @author Gorm
  */
-add_filter( 'dss_logo_url', function($url) { 
-	return get_stylesheet_directory_uri() . '/images/riksvapen.jpg';
-});
-
-
-
+function pwcc_live_reload_js() {
+?>
+<script>document.write('<script src="http://localhost:35729/livereload.js?snipver=1"></' + 'script>')</script>
+<?php
+}
+/** only add if on a .dev domain */
+if ( substr( $_SERVER["SERVER_NAME"], -4) == '.dev' ) {
+	add_action('wp_footer', 'pwcc_live_reload_js');
+}
 
 ?>

16be3943f1120594de6fdfd91eb103e06dab2150
gorm <proev@users.noreply.github.com>
2014-02-03 08:43:37 +0100

    Instead of 302 redirecting to error page, fetch it and give a 404. Should work better with regards to SEO

--- mu-plugins/blogg_global_404.php
+++ mu-plugins/blogg_global_404.php
@@ -2,19 +2,29 @@
 /*
 Plugin Name: redirect 404 to home
 Plugin URI: http://soderlind.no/
-Description: 
+Description:
 Version: 0.5
 Author: Per Soderlind
 Author URI: http://soderlind.no/
 Network: true
- 
+
 */
 
 function blogg_global_404() {
 	if ( is_404() ){
-		wp_redirect( network_home_url('/') . '?e=404' );
+		$curl = curl_init();
+		curl_setopt_array( $curl, array(
+		    CURLOPT_RETURNTRANSFER => 1,
+		    CURLOPT_URL => 'http://blogg.regjeringen.no/?e=404'
+		) );
+		$result = curl_exec( $curl );
+		ob_end_clean();
+		ob_start();
+		print $result;
+		ob_end_flush();
 		die();
 	}
 }
 add_action( 'template_redirect', 'blogg_global_404' );
+
 ?>

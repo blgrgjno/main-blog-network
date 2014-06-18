ffffe80c28d0347f4abdf2c0beae1fd48acf53f4
gorm <proev@users.noreply.github.com>
2014-06-18 13:30:29 +0200

    Added favicon

--- themes/dss-framework/header.php
+++ themes/dss-framework/header.php
@@ -29,6 +29,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
